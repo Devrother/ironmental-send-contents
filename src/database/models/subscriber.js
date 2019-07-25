da57edbe59215f8e83b9a396788212cbf50043f4
SeungUkLee <lsy931106@gmail.com>
2019-07-25 12:24:18 +0900

    Refactoring: delete unuse validation logic and mongoose static methods - fixed #7

--- src/database/models/subscriber.js
+++ src/database/models/subscriber.js
@@ -1,5 +1,4 @@
 import mongoose, { Schema } from 'mongoose';
-import Joi from '@hapi/joi'
 
 const SubscriberSchema = new Schema({
   createdAt: { type: Date, default: Date.now },
@@ -19,35 +18,5 @@ const SubscriberSchema = new Schema({
   favoriteTags: [{ type: String }],
   received: [{ type: Schema.Types.ObjectId, ref: 'Interview' }],
 });
-
-SubscriberSchema.statics.updateSubByEmail = function(email, uuid_v4) {
-  return this.findOneAndUpdate(
-    { email },
-    { confirmCode: uuid_v4 },
-    { runValidators: true },
-  ).orFail();
-};
-
-SubscriberSchema.statics.updateByConfirmCode = function(code) {
-  return this.findOneAndUpdate(
-    { confirmCode: code },
-    { confirmCode: null, isCertify: true },
-    { runValidators: true },
-  ).orFail();
-};
-
-export const validateEmail = (body) => {
-  const schema = Joi.object({
-    email: Joi.string().email()
-  })
-  return schema.validate(body)
-}
-
-export const validateConfirmcode = (body) => {
-  const schema = Joi.object({
-    confirmCode: Joi.string().guid()
-  })
-  return schema.validate(body)
-}
 
 export default mongoose.model('Subscriber', SubscriberSchema);
