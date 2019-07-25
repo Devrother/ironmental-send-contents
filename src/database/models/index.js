7d6333a065e6c778f956797e222faf1fcc8048c5
SeungUkLee <lsy931106@gmail.com>
2019-07-25 12:36:35 +0900

    Edit: eslint

--- src/database/models/index.js
+++ src/database/models/index.js
@@ -1,5 +1,3 @@
 export { default as Interview } from './interview';
-export { validateQuery } from './interview';
 export { default as Subscriber } from './subscriber';
-export { validateEmail, validateConfirmcode } from './subscriber';
 export { default as Tag } from './tag';
